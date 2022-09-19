0e2a8d5d0607425c68affdea4dba9cdf2d75449e
Vilmer Johansson <Wilmer7889@gmail.com>
2022-09-19 13:29:31 +0200

    Update PartyInstaller.hpp

--- include/PartyInstaller.hpp
+++ include/PartyInstaller.hpp
@@ -5,9 +5,6 @@
 
 DECLARE_CLASS_CODEGEN(BringBackTheParty, PartyInstaller, ::Zenject::Installer,
                       
-                      public:
-                      
-                        DECLARE_DEFAULT_CTOR();
-                        DECLARE_OVERRIDE_METHOD(void, InstallBindings, il2cpp_utils::il2cpp_type_check::MetadataGetter<&::Zenject::Installer::InstallBindings>::get());
-
+    DECLARE_OVERRIDE_METHOD(void, InstallBindings, il2cpp_utils::il2cpp_type_check::MetadataGetter<&::Zenject::Installer::InstallBindings>::get());
+    DECLARE_DEFAULT_CTOR();
 )
